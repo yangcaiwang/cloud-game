a5fcab1a667e9bb95bf67e2141dc98dda6760b71
yangcaiwang <ycw123129@163.com>
2022-10-17 01:09:41 +0800

    优化

--- cloud-modules/cloud-common/src/main/java/com/springboot/common/converter/attribute/JsonToListInt.java
+++ cloud-modules/cloud-common/src/main/java/com/springboot/common/converter/attribute/JsonToListInt.java
@@ -1,26 +1,26 @@
-package com.springboot.common.converter.attribute;
-
-
-import com.springboot.common.converter.JsonConvert;
-
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
-import java.util.List;
-
-/**
- * 实现json的序列化和反序列化, 只需要对需要转换的属性打上@convet主机即可
- */
-@Converter
-public class JsonToListInt implements AttributeConverter<List<Integer>, String> {
-    @Override
-    public String convertToDatabaseColumn(List<Integer> value) {
-        return JsonConvert.listIntToString(value);
-    }
-
-    @Override
-    public List<Integer> convertToEntityAttribute(String s) {
-        return JsonConvert.stringToListInt(s);
-    }
-}
-
-
+//package com.springboot.common.converter.attribute;
+//
+//
+//import com.springboot.common.converter.JsonConvert;
+//
+//import javax.persistence.AttributeConverter;
+//import javax.persistence.Converter;
+//import java.util.List;
+//
+///**
+// * 实现json的序列化和反序列化, 只需要对需要转换的属性打上@convet主机即可
+// */
+//@Converter
+//public class JsonToListInt implements AttributeConverter<List<Integer>, String> {
+//    @Override
+//    public String convertToDatabaseColumn(List<Integer> value) {
+//        return JsonConvert.listIntToString(value);
+//    }
+//
+//    @Override
+//    public List<Integer> convertToEntityAttribute(String s) {
+//        return JsonConvert.stringToListInt(s);
+//    }
+//}
+//
+//
--- cloud-modules/cloud-common/src/main/java/com/springboot/common/converter/attribute/JsonToMapLongString.java
+++ cloud-modules/cloud-common/src/main/java/com/springboot/common/converter/attribute/JsonToMapLongString.java
@@ -1,23 +1,23 @@
-package com.springboot.common.converter.attribute;
-
-import com.springboot.common.converter.JsonConvert;
-
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
-import java.util.Map;
-
-/**
- * 实现json的序列化和反序列化, 只需要对需要转换的属性打上@convet主机即可
- */
-@Converter
-public class JsonToMapLongString implements AttributeConverter<Map<Long,String>, String> {
-    @Override
-    public String convertToDatabaseColumn(Map<Long, String> map) {
-        return JsonConvert.mapLongStringToString(map);
-    }
-
-    @Override
-    public Map<Long, String> convertToEntityAttribute(String s) {
-        return JsonConvert.stringToMapLongString(s);
-    }
-}
+//package com.springboot.common.converter.attribute;
+//
+//import com.springboot.common.converter.JsonConvert;
+//
+//import javax.persistence.AttributeConverter;
+//import javax.persistence.Converter;
+//import java.util.Map;
+//
+///**
+// * 实现json的序列化和反序列化, 只需要对需要转换的属性打上@convet主机即可
+// */
+//@Converter
+//public class JsonToMapLongString implements AttributeConverter<Map<Long,String>, String> {
+//    @Override
+//    public String convertToDatabaseColumn(Map<Long, String> map) {
+//        return JsonConvert.mapLongStringToString(map);
+//    }
+//
+//    @Override
+//    public Map<Long, String> convertToEntityAttribute(String s) {
+//        return JsonConvert.stringToMapLongString(s);
+//    }
+//}
